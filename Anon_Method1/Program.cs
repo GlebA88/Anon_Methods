d8b14c3e6aec47a55e8546f6b954d69b38a31d81
wsr10 <wsr10@K218PC11>
2022-04-11 10:30:35 +0300

    Добавьте файлы проекта.

---
+++ Anon_Method1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Anon_Method1
+{
+    public delegate void MyDelegate();
+
+    class Program
+    {
+       static void Main (string [] args)
+       {
+            MyDelegate mD = new MyDelegate(delegate { Console.WriteLine("Hello world"); });
+            mD();
+       }
+    
+
+
+    }
+}
